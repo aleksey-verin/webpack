11e11c3573d3093734861f3cea54a3edc7343b82
Aleksey Verin <verevaa@gmail.com>
2024-05-07 12:01:30 +0300

    feat: change webpack config to ts

---
+++ webpack.config.ts
@@ -0,0 +1,102 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlBundlerPlugin from 'html-bundler-webpack-plugin';
+import 'webpack-dev-server';
+// const path = require('path');
+// const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
+
+const isProd = !process.argv.find((str) => str.includes('development'));
+
+const config: webpack.Configuration = {
+
+    mode: isProd ? 'production' : 'development',
+    devtool: isProd ? 'source-map' : 'inline-source-map', 
+    stats: 'normal', // 'verbose' | 'minimal'
+  
+    output: {
+      path: path.join(__dirname, 'dist/'),
+      clean: true
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+  
+    entry: {
+      // define HTML templates here
+      index: './src/pages/home/index.html',  // => dist/index.html
+      'about/index': './src/pages/about/index.html', // => dist/about/index.html
+    },
+    
+    plugins: [
+      new HtmlBundlerPlugin({
+        js: {
+          // output filename of extracted JS from source script loaded in HTML via `<script>` tag
+          filename: 'assets/js/[name].[contenthash:8].js',
+        },
+        css: {
+          // output filename of extracted CSS from source style loaded in HTML via `<link>` tag
+          filename: 'assets/css/[name].[contenthash:8].css',
+        },
+        minify: isProd ? true : 'auto',
+        loaderOptions: {
+          root: path.join(__dirname, 'src'),
+          sources: [
+            {
+              tag: 'meta',
+              attributes: ['content'],
+              // allow to handlen an image in the 'content' attribute of the 'meta' tag
+              // when the 'property' attribute contains one of: 'og:image', 'og:video'
+              filter: ({ attributes }) => {
+                const attrName = 'property';
+                const attrValues = ['og:image', 'og:video']; // allowed values of the property
+                if (!attributes[attrName] || attrValues.indexOf(attributes[attrName]) < 0) {
+                  return false; // return false to disable processing
+                }
+                // return true or undefined to enable processing
+              },
+            },
+          ],
+        },
+      }),
+    ],
+  
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          loader: HtmlBundlerPlugin.loader, // HTML loader
+        },
+        // styles
+        {
+          test: /\.(css|sass|scss)$/,
+          use: ['css-loader', 'sass-loader'],
+        },
+        // images
+        {
+          test: /\.(png|jpe?g|svg|ico)/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/img/[name].[hash:8][ext]',
+          },
+        },
+      ],
+    },
+  
+    // enable HMR with live reload
+    devServer: {
+      open: true,
+      compress: true,
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      watchFiles: {
+        paths: ['src/**/*.*'],
+        options: {
+          usePolling: true,
+        },
+      },
+    },
+
+}
+
+export default config;
